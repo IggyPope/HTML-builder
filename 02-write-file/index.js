ba08c9840f1adca8cd31fdb35526070ba163df3e
IggyPope <altko4egar@gmail.com>
2024-01-17 20:16:35 +0300

    feat: implement "02-write-file" task solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const process = require('process');
+const { stdin, stdout } = process;
+const { createWriteStream } = require('fs');
+const { join } = require('path');
+
+const filePath = join(__dirname, 'text.txt');
+const writeStream = createWriteStream(filePath);
+
+stdout.write('Please, enter the text to be written to text.txt\n');
+
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    process.emit('SIGINT');
+    return;
+  }
+
+  writeStream.write(data);
+});
+
+process.on('SIGINT', () => {
+  writeStream.close();
+  stdout.write('Thank you, have a nice day!');
+
+  process.exit(0);
+});
