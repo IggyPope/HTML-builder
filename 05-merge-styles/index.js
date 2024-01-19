eaee35fd1fd76473ee298a68d66940c0bd8af268
IggyPope <altko4egar@gmail.com>
2024-01-19 03:43:28 +0300

    feat: implement "05-merge-styles" task solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,29 @@
+const { readdir } = require('fs/promises');
+const { pipeline } = require('stream/promises');
+const { createReadStream, createWriteStream } = require('fs');
+const { join, extname } = require('path');
+
+async function mergeStyles(srcDirPath, destFilePath) {
+  const writeStream = createWriteStream(destFilePath);
+
+  writeStream.on('error', console.error);
+
+  const files = await readdir(srcDirPath);
+  const cssFiles = files.filter((file) => extname(file) === '.css');
+
+  for (const cssFile of cssFiles) {
+    const filePath = join(srcDirPath, cssFile);
+    const readStream = createReadStream(filePath);
+
+    readStream.on('end', () => {
+      writeStream.write('\n');
+    });
+
+    await pipeline(readStream, writeStream, { end: false });
+  }
+}
+
+const srcDirPath = join(__dirname, 'styles');
+const destFilePath = join(__dirname, 'project-dist', 'bundle.css');
+
+mergeStyles(srcDirPath, destFilePath).catch(console.error);
